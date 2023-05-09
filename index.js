cac240791ecce08abc7a0db4b1564d2cc7aad44c
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-05-09 23:34:40 +0200

    Oprava JS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log('funguju!');
 
+/*
 const headerArray = [{
     title: 'Jogíni',
     links: ['domů', 'lekce', 'náš tým', 'události', 'kontakt'],
@@ -18,20 +19,22 @@ const imgArray = [{
     src: 'img/yoga-pose.jpg',
 }]
 
+const LinkElement = (link) => {
+    return `
+        <a href="#">${link}</a>
+    `
+}
+
 const Header = (props) => {
     const { title, links } = props
     return `
         <header>
             <h1 class="site-title">${title}</h1>
             <nav> 
-                <a href="#">${links[0]}</a>
-                <a href="#">${links[1]}</a>
-                <a href="#">${links[2]}</a>
-                <a href="#">${links[3]}</a>
-                <a href="#">${links[4]}</a>
+                ${links.map(link => LinkElement(link)).join('')}
             </nav>
         </header>
-        `
+    `
 }
 
 const Intro = (props) => {
@@ -52,10 +55,92 @@ const Pose = (props) => {
 
 const appJogini = (headerArray, introArray, imgArray) => {
     const appElm = document.querySelector('#app')
-    appElm.innerHTML += headerArray.map(oneHead => Header(oneHead))
-    appElm.innerHTML += introArray.map(oneIntro => Intro(oneIntro))
-    appElm.innerHTML += imgArray.map(oneImg => Pose(oneImg))
-
+    let allHTML = ''
+    allHTML += headerArray.map(oneHead => Header(oneHead))
+    allHTML += introArray.map(oneIntro => Intro(oneIntro))
+    allHTML += imgArray.map(oneImg => Pose(oneImg))
+    appElm.innerHTML = allHTML
 }
 
 appJogini(headerArray, introArray, imgArray)
+*/
+
+const header = {
+    title: 'Jogíni',
+    links: ['domů', 'lekce', 'náš tým', 'události', 'kontakt'],
+}
+
+const intro = {
+    heading: 'Vítejte mezi Jogíny',
+    text: `Naše lekce jsou zaměřeny na potřeby klientů, kteří mají odvahu zkusit
+           něco nového. Cvičíme v pomalém tempu s podrobným slovním doprovodem.
+           Postupně se seznámíte se základními principy jógy, jak přístupovat k
+           sobě i ostatním. Krok za krokem objevíte a dostanete pod kontrolu
+           svoje tělo, pocity a emoce.`,
+}
+
+const img = {
+    src: 'img/yoga-pose.jpg',
+}
+
+//
+
+const appElm = document.querySelector('#app')
+
+const Header = (props) => {
+    const { title, links } = props
+    return `
+        <header>
+            <h1 class="site-title">${title}</h1>
+            <nav> 
+                ${links.map(link => `<a href="#">${link}</a>`).join('')}
+            </nav>
+        </header>
+    `
+}
+
+const displayHeader = (headerObject) => {
+    appElm.innerHTML += Header({
+        title: headerObject.title,
+        links: headerObject.links,
+    })
+}
+
+displayHeader(header)
+
+//
+
+const Intro = (props) => {
+    const { heading, text } = props
+    return `
+        <div class="intro">
+            <h2>${heading}</h2>
+            <p>${text}</p>
+        </div>
+    `
+}
+
+const displayIntro = (introObject) => {
+    appElm.innerHTML += Intro({
+        heading: introObject.heading,
+        text: introObject.text,
+    })
+}
+
+displayIntro(intro)
+
+//
+
+const Pose = (props) => {
+    return `
+        <img class="pose" src=${props.src} />
+    `
+}
+
+const displayPose = (poseObject) => {
+    appElm.innerHTML += Pose({
+        src: poseObject.src,
+    })
+}
+
+displayPose(img)
